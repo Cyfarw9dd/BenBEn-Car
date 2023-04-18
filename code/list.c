5214bd86298c58c93cb63a8c85b97318af4b2452
Cyfarwydd <directorluo@qq.com>
2023-04-18 21:26:33 +0800

    list experimental ver

--- code/list.c
+++ code/list.c
@@ -18,7 +18,7 @@ void KeyScan(void)
     #define AlwaysPressTime 1200
     #define debouncing 5
 
-    if(SomeKeyPress_Flag == 0 && !gpio_get_level(KEY1))
+    if(SomeKeyPress_Flag == 0 && (!gpio_get_level(KEY1) || !gpio_get_level(KEY2) || !gpio_get_level(KEY3) || !gpio_get_level(KEY4)))
     {
         SomeKeyPress_Flag = 1;
     }
@@ -33,14 +33,38 @@ void KeyScan(void)
             {
                 KeyPressNum = 1;
             }
+            if(gpio_get_level(KEY2))
+            {
+                KeyPressNum = 2;
+            }
+            if(gpio_get_level(KEY3))
+            {
+                KeyPressNum = 3;
+            }
+            if(gpio_get_level(KEY4))
+            {
+                KeyPressNum = 4;
+            }
         }
         // 短按
-        if(!gpio_get_level(KEY1) && KeyPressTime < AlwaysPressTime && SomeKeyPress_Flag == 2)
+        if((!gpio_get_level(KEY1) || !gpio_get_level(KEY2) || !gpio_get_level(KEY3) || !gpio_get_level(KEY4)) && KeyPressTime < AlwaysPressTime && SomeKeyPress_Flag == 2)
         {
             SomeKeyPress_Flag = 0;
             if(KeyPressNum == 1)
             {
                 Key1 = onepress;
+            }
+            if(KeyPressNum == 2)
+            {
+                Key2 = onepress;
+            }
+            if(KeyPressNum == 3)
+            {
+                Key3 = onepress;
+            }
+            if(KeyPressNum == 4)
+            {
+                Key4 = onepress;
             }
         }
         // 长按
@@ -50,11 +74,41 @@ void KeyScan(void)
             {
                 Key1 = holdpress;
             }
+            if(KeyPressNum == 2)
+            {
+                Key2 = holdpress;
+            }
+            if(KeyPressNum == 3)
+            {
+                Key3 = holdpress;
+            }
+            if(KeyPressNum == 4)
+            {
+                Key4 = holdpress;
+            }
             if(!gpio_get_level(KEY1))
             {
                 SomeKeyPress_Flag = 0;
                 KeyPressTime = 0;
                 Key1 = nopress;
+            }
+            if(!gpio_get_level(KEY2))
+            {
+                SomeKeyPress_Flag = 0;
+                KeyPressTime = 0;
+                Key2 = nopress;
+            }
+            if(!gpio_get_level(KEY3))
+            {
+                SomeKeyPress_Flag = 0;
+                KeyPressTime = 0;
+                Key3 = nopress;
+            }
+            if(!gpio_get_level(KEY4))
+            {
+                SomeKeyPress_Flag = 0;
+                KeyPressTime = 0;
+                Key4 = nopress;
             }
         }
     }
